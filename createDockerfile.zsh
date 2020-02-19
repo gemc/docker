3f565f4d72f91000a7c7912356a132141db1b543
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2020-02-19 14:21:44 -0500

    added dockerfiles for centos7 and 8

--- createDockerfile.zsh
+++ createDockerfile.zsh
@@ -15,23 +15,33 @@ printUsage() {
         echo " - 2.3"   >&2
         echo " - devel" >&2
         echo " "        >&2
+        exit 0
 }
 
 
 if [ "$#" -ne 1 ]; then
-    printUsage()
+    printUsage
+fi
+version=$1
+
+if [[ "$version" != "devel" && "$version" != "2.3" ]]; then
+    printUsage
 fi
 
-#if [$1 ne "devel"]; then
-#    printUsage()
-#fi
+if [[ "$version" == "devel" ]]; then
+    centos=centos8
+fi
 
-version=$1
+if [[ "$version" != "devel" && "$version" != "2.3" ]]; then
+    printUsage
+fi
 
 rm -f Dockerfile ; touch Dockerfile
 
 echo " "
 echo " Using JLAB_VERSION "$version
+echo " Using centos: "$centos
+echo " "
 echo " Merging BASEOS"
 grep "### START" -A1000 Dockerfile-osbase | grep "### END" -B1000 >> Dockerfile
 
