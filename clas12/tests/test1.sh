3508a70bd1a079f21991f409126e8a96392474ab
maureeungaro <maureeungaro@gmail.com>
2020-03-27 10:15:30 -0400

    added tests for 4.3.2

---
+++ clas12/tests/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# This script will print hello and exit with 0 (success)
+
+echo hello
+
+exit 0
---
+++ clas12/tests/test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# This script will print hello and exit with 1 (failure)
+
+echo hello
+
+exit 1
