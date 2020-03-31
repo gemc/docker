8a4e255741cfa4514d01d4219c626b249774b77c
maureeungaro <maureeungaro@gmail.com>
2020-03-31 18:26:29 -0400

    added generators.test.yml and enabling clas12tags

---
+++ clas12/tests/generators.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Template for generators.
+
+# This script will test these generators with success criteria below:
+#
+# - clasdis. No criteria
+#
+#
+# The script is located in the container at /jlab/generators.sh
+#
+
+clasdis --t 20 25 --docker
+
+
+
+exit 0
--- clas12/tests/test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# This script will print hello and exit with 1 (failure)
-
-echo oh no
-
-exit 1
