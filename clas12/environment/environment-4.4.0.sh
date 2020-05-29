ff1a9fb87557bf4dddf7eb2e2b8d82c561019d2a
ma <maurizio.ungaro@cnu.edu>
2020-05-29 09:41:37 -0400

    tieing CCDB_CONNECTION filename to CLAS12TAG

--- clas12/environment/environment-4.4.0.sh
+++ clas12/environment/environment-4.4.0.sh
@@ -14,7 +14,7 @@ export XERCESC_VERSION=3.2.2
 export XERCESCROOT=/jlab/$JLAB_VERSION/$OSRELEASE/xercesc/$XERCESC_VERSION
 
 # using sqlite
-export CCDB_CONNECTION=sqlite:////jlab/work/ccdb_2020-05-10.sqlite	
+export CCDB_CONNECTION=sqlite:////jlab/work/ccdb_$CLAS12TAG".sqlite"
 
 # sidis, inclusive dis with rad correction, dvcs
 export CLASDIS_PDF=/jlab/work/clas12-mcgen/clasdis/pdf
