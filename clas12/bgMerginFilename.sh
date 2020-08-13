ac9a1143e7da2a2eed0a1a75a2d780e07d5509a2
ma <maurizio.ungaro@cnu.edu>
2020-08-13 09:23:52 -0400

    added bgMerginFilename.sh to get random hipo file

---
+++ clas12/bgMerginFilename.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Returns a random background hipo file in the selected configuration
+# example of command line:
+# bgMerginFilename.sh rga_fall2018  tor-1.00_sol-1.00 45nA_10604MeV
+
+configuration=$1
+fields=$2
+bkmerging=$3
+
+export LD_PRELOAD=/usr/lib64/libXrdPosixPreload.so
+# for onsite
+# baseDir="xroot://sci-xrootd-ib//osgpool/hallb/"
+# for offsite:
+baseDir="xroot://sci-xrootd.jlab.org//osgpool/hallb/"
+xdir=$baseDir"clas12/backgroundfiles/"$configuration"/"$fields"/"$bkmerging"/10k"
+
+NFILES=`ls $xdir | wc | awk '{print $1}'`
+
+# works only if > 100 files
+nzeros="00"
+
+R=$(( $RANDOM % $NFILES ))
+
+if (($R <10))
+then
+	nzeros="0000"
+elif (($R<100))
+then
+	nzeros="000"
+fi
+
+bgfile=$baseDir"clas12/backgroundfiles/"$configuration"/"$fields"/"$bkmerging"/10k/"$nzeros$R".hipo"
+
+echo $bgfile
+
+unset LD_PRELOAD
+
