3eadfeda27dd3394f3d04260d260da07b24ad1e4
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-07-18 15:50:06 -0400

    updated dockerfiles to account for sim/gemc/clas12Tags/cvmfs installation

--- gemc/localSetup.sh
+++ gemc/localSetup.sh
@@ -3,7 +3,17 @@
 source /usr/share/Modules/init/sh
 
 export TERM=xterm-256color
-export SIM_HOME=/usr/local
+export SIM_HOME=templateSim
+
+cd $SIM_HOME
+# if ceInstall is not there, clone it, otherwise pull
+if [ ! -d "ceInstall" ]; then
+  git clone https://github.com/JeffersonLab/ceInstall
+else
+  cd ceInstall
+  git pull
+  cd ..
+fi
 
 source $SIM_HOME/ceInstall/setup.sh install
 
--- localSetup-cvmfs.sh
+++ localSetup-cvmfs.sh
@@ -11,9 +11,16 @@ export TERM=xterm-256color
 
 
 
+
+
+
+
+
+
 alias l='ls -l'
 alias lt='ls -lhrt'
 alias ll='ls -lah'
 alias gist='git status -s | grep -v \?'
 alias gista='git status -s'
+
 
